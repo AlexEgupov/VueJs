c2c6c983b97d6e47e4fd65a64f25a6ed9d64065a
AlexEgupov <alex.egupov7@gmail.com>
2022-07-29 13:26:01 +0300

    feat views, components, router

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
-    <div class="wrapper">
-      <hero-view />
+  <div class="wrapper">
+    <router-view />
 
-      <footer-component />
-    </div>
+    <footer-component />
+  </div>
 </template>
 
 <script>
-import HeroView from '@/views/HeroView.vue'
-import FooterComponent from '@/components/FooterComponent.vue'
+import HeroView from "@/views/HeroView.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
 
 export default {
- components: { HeroView, FooterComponent }
-}
+  components: { HeroView, FooterComponent },
+};
 </script>
 
 <style>
-
 </style>
--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -1,20 +1,30 @@
 <template>
-    <header>
-        <ul class="header d-flex justify-content-center justify-content-md-start flex-wrap">
-            <li class="header__item">
-                <a href="./index.html">
-                    <img src="@/assets/logo/Logo.svg" alt="logo">
-                </a>
-            </li>
-            <li class="header__item">
-                <a href="./coffeepage.html">Our coffee</a>
-            </li>
-            <li class="header__item">
-                <a href="./goodspage.html">For your pleasure</a>
-            </li>
-            <li class="header__item">
-                <a href="./contacts.html">Contact us</a>
-            </li>
-        </ul>
-    </header>
+  <header>
+    <ul
+      class="
+        header
+        d-flex
+        justify-content-center justify-content-md-start
+        flex-wrap
+      "
+    >
+      <li class="header__item">
+        <router-link to="/">
+          <img src="@/assets/logo/Logo.svg" alt="logo" />
+        </router-link>
+      </li>
+      <li class="header__item">
+        <router-link to="/our-coffee">Our coffee</router-link>
+      </li>
+      <li class="header__item">
+        <router-link to="/goods">For your pleasure</router-link>
+      </li>
+      <li class="header__item">
+        <router-link to="/contacts">Contact us</router-link>
+      </li>
+      <li class="header__item">
+        <router-link to="/thanks">Thank You Page</router-link>
+      </li>
+    </ul>
+  </header>
 </template>
